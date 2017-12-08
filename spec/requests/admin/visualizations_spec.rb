7bead51e621f91745d49005684cd64ee172920af
Andrew Carrera <acarrera2@bloomberg.net>
2017-12-08 10:47:06 -0500

    Fixes url for visualizations_spec

--- spec/requests/admin/visualizations_spec.rb
+++ spec/requests/admin/visualizations_spec.rb
@@ -52,19 +52,19 @@ describe Admin::VisualizationsController do
   before(:each) do
     bypass_named_maps
     delete_user_data @user
-    host! "#{@user.username}.localhost.lan"
+    host! "localhost/user/#{@user.username}"
   end
 
   describe 'GET /viz' do
     it 'returns a list of visualizations' do
       login_as(@user, scope: @user.username)
 
-      get "/viz", {}, @headers
+      get public_visualizations_url, {}, @headers
       last_response.status.should == 200
     end
 
     it 'returns 403 if user not logged in' do
-      get "/viz", {}, @headers
+      get public_visualizations_url, {}, @headers
       last_response.status.should == 302
     end
   end # GET /viz
@@ -74,14 +74,14 @@ describe Admin::VisualizationsController do
       id = factory.fetch('id')
       login_as(@user, scope: @user.username)
 
-      get "/viz/#{id}", {}, @headers
+      get public_visualizations_show_url({ id: id }), {}, @headers
       last_response.status.should == 200
     end
 
     it 'redirects to the public view if visualization private' do
       id = factory.fetch('id')
 
-      get "/viz/#{id}", {}, @headers
+      get public_visualizations_show_url({ id: id }), {}, @headers
       follow_redirect!
       last_request.path.should =~ %r{/viz/}
     end
@@ -89,7 +89,7 @@ describe Admin::VisualizationsController do
     it 'keeps the base path (table|visualization) when redirecting' do
       id = table_factory.id
 
-      get "/tables/#{id}", {}, @headers
+      get public_tables_show_bis_url({ id: id }), {}, @headers
       follow_redirect!
       last_request.path.should =~ %r{/tables/}
     end
@@ -104,7 +104,7 @@ describe Admin::VisualizationsController do
           @user.stubs(:builder_enabled?).returns(true)
 
           login_as(@user, scope: @user.username)
-          get "/tables/#{@id}", {}, @headers
+          get public_tables_show_bis_url({ id: @id }), {}, @headers
           last_response.status.should eq 302
           follow_redirect!
           last_request.path.should =~ %r{/dataset/}
@@ -115,7 +115,7 @@ describe Admin::VisualizationsController do
           @user.stubs(:builder_enabled?).returns(false)
 
           login_as(@user, scope: @user.username)
-          get "/tables/#{@id}", {}, @headers
+          get public_tables_show_bis_url({ id: @id }), {}, @headers
           last_response.status.should eq 200
         end
 
@@ -124,7 +124,7 @@ describe Admin::VisualizationsController do
           @user.stubs(:builder_enabled?).returns(false)
 
           login_as(@user, scope: @user.username)
-          get "/tables/#{@id}", {}, @headers
+          get public_tables_show_bis_url({ id: @id }), {}, @headers
           last_response.status.should eq 200
         end
       end
@@ -138,7 +138,7 @@ describe Admin::VisualizationsController do
           @user.stubs(:builder_enabled?).returns(true)
 
           login_as(@user, scope: @user.username)
-          get "/viz/#{@id}", {}, @headers
+          get public_visualizations_show_url({ id: @id }), {}, @headers
           last_response.status.should eq 302
           follow_redirect!
           last_request.path.should =~ %r{/builder/}
@@ -149,7 +149,7 @@ describe Admin::VisualizationsController do
           @user.stubs(:builder_enabled?).returns(false)
 
           login_as(@user, scope: @user.username)
-          get "/viz/#{@id}", {}, @headers
+          get public_visualizations_show_url({ id: @id }), {}, @headers
           last_response.status.should eq 200
         end
 
@@ -158,7 +158,7 @@ describe Admin::VisualizationsController do
           @user.stubs(:builder_enabled?).returns(false)
 
           login_as(@user, scope: @user.username)
-          get "/viz/#{@id}", {}, @headers
+          get public_visualizations_show_url({ id: @id }), {}, @headers
           last_response.status.should eq 200
         end
 
@@ -168,7 +168,7 @@ describe Admin::VisualizationsController do
           Carto::Visualization::any_instance.stubs(:uses_vizjson2?).returns(true)
 
           login_as(@user, scope: @user.username)
-          get public_visualizations_show_path(id: @id), {}, @headers
+          get public_visualizations_show_url(id: @id), {}, @headers
           last_response.status.should eq 200
         end
 
@@ -179,7 +179,7 @@ describe Admin::VisualizationsController do
           Carto::Visualization.find(@id).layers.create(kind: 'gmapsbase')
 
           login_as(@user, scope: @user.username)
-          get public_visualizations_show_path(id: @id), {}, @headers
+          get public_visualizations_show_url(id: @id), {}, @headers
           last_response.status.should eq 200
         end
 
@@ -192,14 +192,14 @@ describe Admin::VisualizationsController do
           visualization.store
 
           login_as(@user, scope: @user.username)
-          get public_visualizations_embed_map_path(id: @id), {}, @headers
+          get public_visualizations_embed_map_url(id: @id), {}, @headers
           last_response.status.should eq 200
 
           visualization.version = 3
           visualization.store
 
           login_as(@user, scope: @user.username)
-          get public_visualizations_embed_map_path(id: @id), {}, @headers
+          get public_visualizations_embed_map_url(id: @id), {}, @headers
           last_response.status.should eq 302
         end
       end
@@ -210,7 +210,7 @@ describe Admin::VisualizationsController do
     it 'returns 404 for private tables' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PRIVATE).id
 
-      get "/tables/#{id}/public/table", {}, @headers
+      get public_table_table_url({ id: id }), {}, @headers
       last_response.status.should == 404
     end
   end
@@ -219,7 +219,7 @@ describe Admin::VisualizationsController do
     it 'returns 404 for private maps' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PRIVATE).table_visualization.id
 
-      get "/viz/#{id}/protected_public_map", {}, @headers
+      get protected_public_map_url(id: id), {}, @headers
       last_response.status.should == 404
     end
   end
@@ -228,7 +228,7 @@ describe Admin::VisualizationsController do
     it 'returns 404 for private maps' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PRIVATE).table_visualization.id
 
-      get "/viz/#{id}/protected_embed_map", {}, @headers
+      get protected_embed_map_url(id: id), {}, @headers
       last_response.status.should == 404
     end
   end
@@ -237,14 +237,14 @@ describe Admin::VisualizationsController do
     it 'returns 403 for private maps' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PRIVATE).table_visualization.id
 
-      get "/viz/#{id}/public_map", {}, @headers
+      get public_visualizations_public_map_url(id: id), {}, @headers
       last_response.status.should == 403
     end
 
     it 'returns proper surrogate-keys' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PUBLIC).table_visualization.id
 
-      get "/viz/#{id}/public_map", {}, @headers
+      get public_visualizations_public_map_url(id: id), {}, @headers
       last_response.status.should == 200
       last_response.headers["Surrogate-Key"].should_not be_empty
       last_response.headers["Surrogate-Key"].should include(CartoDB::SURROGATE_NAMESPACE_PUBLIC_PAGES)
@@ -259,8 +259,8 @@ describe Admin::VisualizationsController do
 
       vis_id = new_table({user_id: user_a.id, privacy: ::UserTable::PRIVACY_PUBLIC}).save.reload.table_visualization.id
 
-      host! "#{org.name}.localhost.lan"
-      get "/viz/#{vis_id}/public_map", @headers
+      host! "localhost/user/#{org.name}"
+      get public_visualizations_public_map_url(id: vis_id), @headers
       last_response.status.should == 200
     end
 
@@ -279,7 +279,7 @@ describe Admin::VisualizationsController do
     it 'serves X-Frame-Options: DENY' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PUBLIC).table_visualization.id
 
-      get "/viz/#{id}/public_map", {}, @headers
+      get public_visualizations_public_map_url(id: id), {}, @headers
       last_response.status.should == 200
       last_response.headers['X-Frame-Options'].should == 'DENY'
     end
@@ -308,14 +308,14 @@ describe Admin::VisualizationsController do
     it 'returns public data for a table visualization' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PUBLIC).table_visualization.id
 
-      get "/viz/#{id}/public", {}, @headers
+      get public_visualization_url(id: id), {}, @headers
       last_response.status.should == 200
     end
 
     it 'returns a 404 if table is private' do
       id = table_factory.table_visualization.id
 
-      get "/viz/#{id}/public", {}, @headers
+      get public_visualization_url(id: id), {}, @headers
       last_response.status.should == 404
     end
 
@@ -324,7 +324,7 @@ describe Admin::VisualizationsController do
       derived_visualization = FactoryGirl.create(:derived_visualization, user_id: @user.id, map_id: map.id)
       id = derived_visualization.id
 
-      get "/viz/#{id}/public", {}, @headers
+      get public_visualization_url(id: id), {}, @headers
       last_response.status.should == 302
       follow_redirect!
       last_response.status.should == 200
@@ -334,7 +334,7 @@ describe Admin::VisualizationsController do
 
   describe 'GET /tables/:id/embed_map' do
     it 'returns 404 for nonexisting tables when table name is used' do
-      get "/tables/tablethatdoesntexist/embed_map", {}, @headers
+      get public_tables_embed_map_url(id: "tablethatdoesntexist"), {}, @headers
       last_response.status.should == 404
     end
   end
@@ -344,7 +344,7 @@ describe Admin::VisualizationsController do
       table = table_factory(privacy: ::UserTable::PRIVACY_PUBLIC)
       name = table.table_visualization.name
 
-      get "/viz/#{URI::encode(name)}/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: URI::encode(name)), {}, @headers
       last_response.status.should == 200
       last_response.headers["X-Cache-Channel"].should_not be_empty
       last_response.headers["X-Cache-Channel"].should include(table.name)
@@ -356,7 +356,7 @@ describe Admin::VisualizationsController do
 
     it 'renders embed map error page if visualization private' do
       table = table_factory
-      put "/api/v1/tables/#{table.id}?api_key=#{@api_key}",
+      put api_v1_tables_show_url({ id: table.id, api_key: @api_key}),
         { privacy: 0 }.to_json, @headers
 
       name = table.table_visualization.name
@@ -364,7 +364,7 @@ describe Admin::VisualizationsController do
 
       login_as(@user, scope: @user.username)
 
-      get "/viz/#{name}/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: name), {}, @headers
       last_response.status.should == 403
       last_response.body.should include("Looks like this map is set as private or no longer exists")
     end
@@ -372,7 +372,7 @@ describe Admin::VisualizationsController do
     it 'renders embed map error when an exception is raised' do
       login_as(@user, scope: @user.username)
 
-      get "/viz/220d2f46-b371-11e4-93f7-080027880ca6/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: "220d2f46-b371-11e4-93f7-080027880ca6"), {}, @headers
       last_response.status.should == 404
     end
 
@@ -380,7 +380,7 @@ describe Admin::VisualizationsController do
       table = table_factory(privacy: ::UserTable::PRIVACY_PUBLIC)
       name = table.table_visualization.name
 
-      get "/viz/#{URI::encode(name)}/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: URI::encode(name)), {}, @headers
       last_response.status.should == 200
       last_response.headers.include?('X-Frame-Options').should_not == true
     end
@@ -392,7 +392,7 @@ describe Admin::VisualizationsController do
       embed_redis_cache = EmbedRedisCache.new
 
       embed_redis_cache.get(id, https=false).should == nil
-      get "/viz/#{id}/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: id), {}, @headers
       last_response.status.should == 200
 
       # The https key/value pair should be differenent
@@ -403,7 +403,7 @@ describe Admin::VisualizationsController do
       embed_redis_cache.get(id, https=false).should_not be_nil
       first_response = last_response
 
-      get "/viz/#{id}/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: id), {}, @headers
       last_response.status.should == 200
       # Headers of both responses should be the same excluding some
       remove_changing = lambda {|h| h.reject {|k, v| ['X-Request-Id', 'X-Runtime'].include?(k)} }
@@ -414,7 +414,7 @@ describe Admin::VisualizationsController do
     it 'doesnt serve X-Frame-Options: DENY on embedded' do
       id = table_factory(privacy: ::UserTable::PRIVACY_PUBLIC).table_visualization.id
 
-      get "/viz/#{id}/embed_map", {}, @headers
+      get public_visualizations_embed_map_url(id: id), {}, @headers
       last_response.status.should == 200
       last_response.headers.include?('X-Frame-Options').should_not == true
     end
@@ -424,14 +424,14 @@ describe Admin::VisualizationsController do
     it 'renders the view by passing a visualization name' do
       login_as(@user, scope: @user.username)
 
-      get "/viz/track_embed", {}, @headers
+      get public_visualizations_track_embed_url, {}, @headers
       last_response.status.should == 200
     end
 
     it 'doesnt serve X-Frame-Options: DENY for track_embed' do
       login_as(@user, scope: @user.username)
 
-      get "/viz/track_embed", {}, @headers
+      get public_visualizations_track_embed_url, {}, @headers
       last_response.status.should == 200
       last_response.headers.include?('X-Frame-Options').should_not == true
     end
@@ -441,13 +441,16 @@ describe Admin::VisualizationsController do
     it 'returns 404' do
       login_as(@user, scope: @user.username)
 
-      get "/viz/220d2f46-b371-11e4-93f7-080027880ca6?api_key=#{@api_key}", {}, @headers
+      get public_visualizations_show_url(id: "220d2f46-b371-11e4-93f7-080027880ca6", api_key: @api_key), {}, @headers
       last_response.status.should == 404
 
-      get "/viz/220d2f46-b371-11e4-93f7-080027880ca6/public?api_key=#{@api_key}", {}, @headers
+      # Need to create the public url...but public_visualizations_url 
+      # is also used for /viz which leads to the wrong url to be generated
+      url = public_visualizations_show_url(id: "220d2f46-b371-11e4-93f7-080027880ca6") + "/public?api_key=#{@api_key}"
+      get url, {}, @headers
       last_response.status.should == 404
 
-      get "/viz/220d2f46-b371-11e4-93f7-080027880ca6/embed_map?api_key=#{@api_key}", {}, @headers
+      get public_visualizations_embed_map_url(id: "220d2f46-b371-11e4-93f7-080027880ca6", api_key: @api_key), {}, @headers
       last_response.status.should == 404
     end
   end # non existent visualization
@@ -518,6 +521,7 @@ describe Admin::VisualizationsController do
 
       login_host(user_b, org)
 
+      puts CartoDB.url(@mock_context, 'public_table', { id: vis.name }, user_a)
       get CartoDB.url(@mock_context, 'public_table', { id: vis.name }, user_a)
       last_response.status.should be(404)
 
@@ -641,7 +645,7 @@ describe Admin::VisualizationsController do
     it 'invokes user metadata redis caching' do
       Carto::UserDbSizeCache.any_instance.expects(:update_if_old).with(@user).once
       login_as(@user, scope: @user.username)
-      get dashboard_path, {}, @headers
+      get dashboard_url, {}, @headers
     end
   end
 
@@ -696,7 +700,7 @@ describe Admin::VisualizationsController do
 
   def login_host(user, org = nil)
     login_as(user, scope: user.username)
-    host! "#{org.nil? ? user.username : org.name}.localhost.lan"
+    host! "localhost/user/#{org.nil? ? user.username : org.name}"
   end
 
   def follow_redirects(limit = 10)
@@ -716,8 +720,8 @@ describe Admin::VisualizationsController do
       type:         'derived'
     }
 
-    with_host "#{owner.username}.localhost.lan" do
-      post "/api/v1/viz?api_key=#{owner.api_key}", payload.to_json
+    with_host "localhost/user/#{owner.username}" do
+      post api_v1_visualizations_index_url({ api_key: owner.api_key }), payload.to_json
     end
 
 
