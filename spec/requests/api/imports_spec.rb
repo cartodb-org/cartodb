35994e8abfa37a44ad15cb05929fc04fa91b0146
Andrew Carrera <acarrera2@bloomberg.net>
2017-12-11 08:51:31 -0500

    Changes hardcoded url to carto api in imports_spec

--- spec/requests/api/imports_spec.rb
+++ spec/requests/api/imports_spec.rb
@@ -11,7 +11,7 @@ describe "Imports API" do
   before(:each) do
     bypass_named_maps
     delete_user_data @user
-    host! "localhost/user/#{@user.username}"
+    host! CartoDB.base_url(@user.username).sub!(/^https?\:\/\//, '')
   end
 
   after(:all) do
