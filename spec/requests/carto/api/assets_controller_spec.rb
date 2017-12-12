f869a84c401c964597502ad7e2b66588040ab372
Andrew Carrera <acarrera2@bloomberg.net>
2017-12-12 16:19:42 -0500

    Fixes url for assets_controller_spec

--- spec/requests/carto/api/assets_controller_spec.rb
+++ spec/requests/carto/api/assets_controller_spec.rb
@@ -13,7 +13,7 @@ describe Carto::Api::AssetsController do
     before(:each) do
       bypass_named_maps
       delete_user_data @user
-      host! "#{@user.username}.localhost.lan"
+      host! CartoDB.base_url(@user.username).sub!(/^https?\:\/\//, '')
     end
 
     after(:all) do
